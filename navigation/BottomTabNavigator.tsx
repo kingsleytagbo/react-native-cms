aa432f4865feebc996007a415242b66ca5913315
ktagbo <kingsleytagbo@gmail.com>
2020-10-21 13:36:15 -0500

    testing

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -77,7 +77,7 @@ function LoginNavigator() {
       <LoginStack.Screen
         name="Login"
         component={LoginScreen}
-        options={{ headerTitle: 'React Native - AccountLogin' }}
+        options={{ headerTitle: 'React Native - Account Login' }}
       />
     </LoginStack.Navigator>
   );
